8990181f33054807c83569b27f15510e17567aa1
cwooz <woosley.christopher@gmail.com>
2017-04-18 00:51:16 -0400

    finished for the night

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+  def index
+    @content_first = "This is some sample text for the Ruby App"
+    @content_second = "Find me in app/views/posts/index.html.erb"
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+
+end
