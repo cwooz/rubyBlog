79bfcd401c0b83c0e7ee092cdfe0629dbcdc6365
cwooz <woosley.christopher@gmail.com>
2017-04-20 16:09:03 -0400

    finish for the day

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,26 +1,28 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show, :edit, :update, :destroy]
+  #? before_action :set_category, only: [:show, :edit, :update, :destroy]
 
   # GET /categories
   # GET /categories.json
   def index
     @categories = Category.all
 
-        respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @categories }
-    end
+    # respond_to do |format|
+    #   format.html # index.html.erb
+    #   format.json { render json: @categories }
+    # end
   end
 
   # GET /categories/1
   # GET /categories/1.json
   def show
     @category = Category.find(params[:id])
+    @title = @category.name
+    @posts = @category.posts
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @categorie }
-    end
+    # respond_to do |format|
+    #   format.html # index.html.erb
+    #   format.json { render json: @categorie }
+    # end
   end
 
 
