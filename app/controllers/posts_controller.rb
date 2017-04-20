a89ef8faf823cfc61c42873212528eebd0fe0773
cwooz <woosley.christopher@gmail.com>
2017-04-20 15:36:38 -0400

    prep for active admin

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,32 +12,32 @@ class PostsController < ApplicationController
     @category = Category.all
   end
 
-  def create
-    @post = Post.new(params[:post])
-    if @post.save
-      redirect_to posts_path, :notice => "Your post has been saved"
-    else
-      render "new"
-    end
-  end
+  # def create
+  #   @post = Post.new(params[:post])
+  #   if @post.save
+  #     redirect_to posts_path, :notice => "Your post has been saved"
+  #   else
+  #     render "new"
+  #   end
+  # end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
+  # def edit
+  #   @post = Post.find(params[:id])
+  # end
 
-  def update
-    @post = Post.find(params[:id])
-    if @post.update_attributes(params[:post])
-      redirect_to post_path, :notice => "Your post has been updated"
-    else
-      render "edit"
-    end
-  end
+  # def update
+  #   @post = Post.find(params[:id])
+  #   if @post.update_attributes(params[:post])
+  #     redirect_to post_path, :notice => "Your post has been updated"
+  #   else
+  #     render "edit"
+  #   end
+  # end
   
-  def destroy
-    @post = Post.find(params[:id])
-    @post.destroy
-    redirect_to posts_path, :notice => "Your post has been deleted"
-  end
+  # def destroy
+  #   @post = Post.find(params[:id])
+  #   @post.destroy
+  #   redirect_to posts_path, :notice => "Your post has been deleted"
+  # end
 
 end
